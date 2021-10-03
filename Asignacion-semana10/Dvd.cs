3bba06b91f74546d5ce4de413d646fb9087024e0
ismael-15 <88355594+ismael-15@users.noreply.github.com>
2021-10-02 18:08:11 -0600

    Add files via upload

---
+++ Asignacion-semana10/Dvd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignacion_semana10
+{
+    class Dvd
+    {
+        public string _Dvd;
+
+
+        public string getDvd()
+        {
+            return _Dvd;
+        }
+        public void setDvd(string Dvd)
+        {
+            Dvd = _Dvd;
+        }
+    }
+}
---
+++ Asignacion-semana10/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asignacion_semana10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // instancia
+            Cd cd = new Cd();
+
+            cd.settitle("MINUTOS");
+            cd.setcomment("esta bonita ");
+            cd._playinTime = "2 horas";
+            cd._artist = "RICARDO ARJONA";
+            cd._numberOfTracks = "10";
+
+            MessageBox.Show(cd.gettitle());
+            MessageBox.Show(cd.getcomment());
+            MessageBox.Show(cd._playinTime);
+            MessageBox.Show(cd.getartist());
+            MessageBox.Show(cd.getnumberOfTracsk());
+            MessageBox.Show(cd.print());
+
+            
+
+
+ 
+        }
+    }
+}
